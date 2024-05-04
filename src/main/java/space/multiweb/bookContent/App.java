1a3fe62a126c1345a89a57d59f3952d4ee6b4c03
Zablotskyi <zablotskyivv@gmail.com>
2024-05-04 17:31:49 +0300

    started to perform the project BookContent

---
+++ src/main/java/space/multiweb/bookContent/App.java
@@ -0,0 +1,11 @@
+package space.multiweb.bookContent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
